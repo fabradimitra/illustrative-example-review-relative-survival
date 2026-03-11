fc286f8498bbea57a93d17023951cf1ac8f167fa
fabradimitra <fabriziodimari@gmail.com>
2026-03-11 13:14:41 +0100

    Finilized the first plot comparing all estimators.

--- main.R
+++ main.R
@@ -3,29 +3,69 @@ data(colrec)
 summary(colrec)
 # Let us first compare all estimators in the whole population of rectum cancer patients
 sub.rec <- subset(colrec, site == "rectum")
+# Censor all patients at 10 years of follow-up
+ind_10plus <- which(sub.rec$time > 365.241 * 10)
+sub.rec$time[ind_10plus] <- 365.241 * 10
+sub.rec$stat[ind_10plus] <- 0
+# Compute the relative survival estimates using the different estimators
 rs_e1 <- rs.surv(Surv(time,stat)~1,
-  rmap = list(age = age, sex = ))
+  rmap = list(age = age, sex = sex, year = diag),
+  method = "ederer1",
+  ratetable = slopop,
+  data = sub.rec)
+rs_e2 <- rs.surv(Surv(time,stat)~1,
+  rmap = list(age = age, sex = sex, year = diag),
+  method = "ederer2",
+  ratetable = slopop,
+  data = sub.rec)
+rs_hak <- rs.surv(Surv(time,stat)~1,
+  rmap = list(age = age, sex = sex, year = diag),
+  method = "hakulinen",
+  ratetable = slopop,
+  data = sub.rec)
+rs_pp <- rs.surv(Surv(time,stat)~1,
+  rmap = list(age = age, sex = sex, year = diag),
+  method = "pohar-perme",
+  ratetable = slopop,
+  data = sub.rec)
+# Plot four panels with a shared legend (Okabe-Ito palette)
+par(mfrow = c(2, 2), oma = c(0, 0, 3, 0))
+
+plot(rs_e1, xscale = 365.241,
+     xlab = "Follow-up time in years",
+     ylab = "Rel. surv. ratio",
+     main = "Ederer I",
+     ylim = c(0, 1),
+     lwd = 1.5)
+par(xpd = FALSE)
+grid(col = "black")
+plot(rs_e2, xscale = 365.241,
+     xlab = "Follow-up time in years",
+     ylab = "Obs. net surv.",
+     main = "Ederer II",
+     ylim = c(0, 1),
+     lwd = 1.5)
+par(xpd = FALSE)
+grid(col = "black")
+plot(rs_hak, xscale = 365.241,
+     xlab = "Follow-up time in years",
+     ylab = "Rel. surv. ratio",
+     main = "Hakulinen",
+     ylim = c(0, 1),
+     lwd = 1.5)
+par(xpd = FALSE)
+grid(col = "black")
+plot(rs_pp, xscale = 365.241,
+     xlab = "Follow-up time in years",
+     ylab = "Net surv.",
+     main = "Pohar-Perme",
+     ylim = c(0, 1),
+     lwd = 1.5)
+par(xpd = FALSE)
+grid(col = "darkgray")
 
 # Let us consider the subset of rectum cancer patients diagnosed in 1994 and 2000
+
 sub.rec.94 <- subset(colrec, site == "rectum" & diag >= as.Date("1994-01-01") & diag < as.Date("1995-01-01"))
 sub.rec.00 <- subset(colrec, site == "rectum" & diag >= as.Date("2000-01-01") & diag < as.Date("2001-01-01"))
 
-rs_e2 <- rs.surv(Surv(follow.up.time.in.days,
-                      Follow.up.status)~1,
-                 rmap = list(age = Age.at.diagnosis*365.241,
-                             sex = Sex, year = as.Date(paste(Year.of.diagnosis,
-                                                             Month.of.diagnosis,
-                                                             Day.of.Diagnosis,sep = "-"))), 
-                 method = "ederer2",
-                 ratetable = slopop,
-                 data = res$data)
-
-par(mfrow = c(4,4), oma=c(2,0,2,0), mar=c(1, 4, 4, 1) + 0.1)
-plot(rs_e2, xscale=365.241, ylim=c(0,1), xlim = c(0,20*365.241))
-grid(col = "black")
-mtext("Period of diagnosis", line=0, side=3, outer=TRUE, cex=1,font=2)
-title("1993-1997", ylab = "Age 0-54",
-      font.lab=2, 
-      cex.main = 1.2,
-      cex.lab = 1.2)
-summary(slopop)
